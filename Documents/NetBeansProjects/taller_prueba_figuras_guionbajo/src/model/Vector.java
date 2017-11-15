f862e9f884e59c69362a92b1031c07ffbecf0664
ccastrom <claudio_jr@live.cl>
2017-11-14 21:56:10 -0300

    Se procede a crear lineas que debiese representaran las figuras geometricas,en un futuro. se añade la clase vector y una clase main para su ejecucion.

---
+++ Documents/NetBeansProjects/taller_prueba_figuras_guionbajo/src/model/Vector.java
@@ -0,0 +1,27 @@
+
+package model;
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Vector extends JPanel {
+    
+    public void paint(Graphics linea){
+        
+        linea.setColor(Color.GREEN);
+        linea.drawLine(0,0,500,500);
+        linea.drawLine(0,0,500,520);
+        linea.drawLine(0,0,500,540);
+        linea.drawLine(0,0,500,560);
+        linea.drawLine(0,0,500,580);
+        linea.drawLine(0,0,500,600);
+        linea.drawLine(0,0,500,620);
+        linea.drawLine(0,0,500,660);
+        linea.drawLine(0,0,500,680);
+        linea.drawLine(0,0,500,700);
+        linea.drawLine(0,0,500,720);
+        
+    }
+    
+    
+}
---
+++ Documents/NetBeansProjects/taller_prueba_figuras_guionbajo/src/model/Vectormain.java
@@ -0,0 +1,25 @@
+
+package model;
+import javax.swing.*;//Crea ventana basica
+import java.awt.*; //Modo grafico.
+
+
+public class Vectormain {
+
+    
+    public static void main(String[] args) {
+        //CREACIÓN DE VENTANA BASICA
+        Vector vec= new Vector();
+        
+        JFrame vector = new JFrame("Dibujo");
+        vector.setVisible(true);
+        vector.setSize(500,500);
+        vector.setLocationRelativeTo(null);
+        vector.setBackground(Color.black);
+        vector.add(vec);
+        vector.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+    }
+    
+}
